8c45866568921954f435d79a55621c0b8fd41883
e-h-dev <ebaal8226@gmail.com>
2025-09-16 01:18:47 +0100

    Create javascript file, and function to open and close submit section

---
+++ index.js
@@ -0,0 +1,9 @@
+function createSubmit(){
+    let overLay = document.getElementById("overlay");
+    overLay.style = "display:block";
+}
+
+function closeSubmit(){
+    let overLay = document.getElementById("overlay");
+    overLay.style = "display:none";
+}
