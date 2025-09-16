e00b71d99ed38db69de26fdeaba4c1eb8f91cf70
e-h-dev <ebaal8226@gmail.com>
2025-09-16 10:57:49 +0100

    Create function to clear text before loading selected text

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,14 @@
+let overLay = document.getElementById("overlay");
+let mainLoad = document.getElementById("main-load");
+
 function createSubmit(){
-    let overLay = document.getElementById("overlay");
     overLay.style = "display:block";
 }
 
 function closeSubmit(){
-    let overLay = document.getElementById("overlay");
     overLay.style = "display:none";
+}
+
+function clearMainText(){
+    mainLoad.style = "display:none";
 }
